516776d040c5207cf6deaf10f34f311a6a9994c1
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-09 10:01:18 -0700

    task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,25 @@
-{ #include <stdio.h>
+#include <stdio.h>
+
+#include <stdlib.h>
 
 
 
-	int main(void) { write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 61);
+/**
+ *
+ *  * main - Entry point
+ *
+ *   * Return: Always 1 (Success)
+ *
+ *    */
 
+int main(void)
 
+{
 
-		return (1);
+		write(STDOUT_FILENO, "and that piece of art is useful
 
-	} }
+							\" - Dora Korpar, 2015-10-19\n", 59);
+
+			return (1);
+
+}
