fe709f72142642e63779045066042085d4104623
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-09 04:17:18 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,21 @@
-#!/bin/bash
 #include <stdio.h>
-main() {
 
-puts("Programming is like building a multilingual puzzle");
-	return 0;
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+	puts("Programming is like building a multilingual puzzle"\n);
+       	return (0);
+
 }
