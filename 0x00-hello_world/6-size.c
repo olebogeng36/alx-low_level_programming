fe362b87f8e32d8668f06c7c3fc94a503c1d650f
olebogeng36 <106256575+olebogeng36@users.noreply.github.com>
2022-09-09 06:29:34 -0700

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,21 @@
-#include <stdio.h>
 /**
-*main - print the size
-*
-*return 0
-*/
+ * main - print the string in the function
+ *
+ *description: using the main funtion
+ *this program
+ */
 int main(void)
 {
-  
-  return 0;
-}
+char c;
+int i;
+long li;
+long long lli;
+float f;
+
+printf("size of a char: %1d byte(s)\n", sizeof(c));
+printf("size of a int: %1d byte(s)\n", sizeof(i));
+printf("size of a long int: %1d byte(s)\n", sizeof(li));
+printf("size of a long long: %1d byte(s)\n", sizeof(lli));
+printf("size of a float: %1d byte(s)\n", sizeof(f));
+
+return 0;
