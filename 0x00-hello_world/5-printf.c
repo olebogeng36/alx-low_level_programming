88175e7370738093955f32bee3efbfc46fa403c9
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-09 04:48:29 -0700

    task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	    /**
+	     *      * print the following
+	     *           * printf
+	     *                * return 0;
+	     *                     **/
+	     printf("with proper grammar, but the outcome is a piece of art,");
+	          return 0;
+}
