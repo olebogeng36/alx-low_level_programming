bdaf4c28cd37948f5584d99c3d47dfd4d486ffbd
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-11 12:08:49 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print the output of putchar
+ *
+ * Description: main function
+ * print the program "putchar from 0 to base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
