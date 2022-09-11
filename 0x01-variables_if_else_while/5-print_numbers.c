8c00d46a35aea79a6db5e568fa9451c76c212a96
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-11 10:57:35 -0700

    task5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print the starting from 0 to 10
+ *
+ * Description: main funtion
+ * this program prints "Numbers to base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
