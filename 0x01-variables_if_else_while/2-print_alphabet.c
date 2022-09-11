2bc62cb6281cf830327c83cf10164f1e87aee539
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-11 04:51:28 -0700

    task2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - entry
@@ -12,8 +14,7 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
 		putchar(ch);
-	}
+		putchar('\n');
 	return (0);
 }
