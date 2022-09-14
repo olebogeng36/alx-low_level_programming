95136df922743a38974f45481aa52778a6bec284
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-13 17:35:58 -0700

    task11

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Adds two integers.
+ * @num1: The first integer to be added.
+ * @num2: The second integer to be added.
+ * Description: main function
+ * Return: The result of the addition.
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_to_98 - Prints all natural numbers from input to 98,
+ *in order separated by a comma followed by a space
+ *Descripton :main function for n
+ *
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
