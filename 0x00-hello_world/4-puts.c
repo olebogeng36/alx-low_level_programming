d4e76699fe0ba0b6ec8ec2c13b6b397a5bbaafbb
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-17 00:11:04 -0700

    tasjk

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 
 /**
   * main - print the string in the put function
-  *
   * Description: using the main function
   * this program prints "Programming is like building a multilingual puzzle
   * Return: 0
