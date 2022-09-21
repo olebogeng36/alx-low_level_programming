0e554f8a783b6f3bc112741c03b3d1ced09eb51e
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-21 15:53:53 -0700

    adv

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *
+ *
+ *
+ *
+ */
+int _atoi(char *s)
+{
+	int str[] = "98";
+	
+	*s endPtr;
+	
+	int x = strtol(str, &endPtr, 10);
+	printf("%d", x);
+	
+	return (0);
+}
