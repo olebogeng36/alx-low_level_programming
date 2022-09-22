6199296784a148c923bfd5d775f4d6aa48541d6f
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-22 13:34:52 -0700

    task

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,12 +12,6 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int m;
-	int dest_len = strlen(dest);
-
-	for (m = 0; m < n && src[m] != '\0'; m++)
-		dest[dest_len + m] = src[m];
-		dest[dest_len + m] = '\0';
-
+	strncat(dest, src, n);
 	return (dest);
 }
