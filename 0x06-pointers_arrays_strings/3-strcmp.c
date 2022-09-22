9291a510937117e434bdba0bef8decc7a1192ab3
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-22 12:57:07 -0700

    task

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: description
+ * @s2: string
+ * @n: value
+ * Return: (0)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int fire;
+
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	{
+		if (s1[i] != s2[i])
+			break;
+		if else(s1[i] == s2[i]
+				continue;
+	}
+	return (0);
+}
