fef2c385a4fbf37808093269da7cb0eadf9c94cb
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-22 14:05:16 -0700

    task

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - reversing aray
+ * @arra a
+ * @n number of element
+ * Return; nothing
+ */
+
+void reverse_array(int *a, int n);
+{
+
+}
