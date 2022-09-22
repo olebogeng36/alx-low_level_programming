ad4741ef838a3c0d051e53fcd24feb7c36028e17
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-22 16:33:20 -0700

    task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,37 +3,19 @@
 
 /**
  * _strcmp - compare two strings
- * @s1: description
- * @s2: string
- * @n: value
- * Return: (0)
+ * @s1: an input string
+ * @s2: an input string
+ * Return: The difference s1 - s2
  */
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, diff = 0;
-
-	while (1)
+	while (*s1 && *s2)
 	{
-		if (s1[i] == '\0' && s2[i] == '\0')
-			break;
-		else if (s1[i] == '\0')
-		{
-			diff = s2[i];
-			break;
-		}
-		else if (s2[i] == '\0')
-		{
-			diff = s1[i];
-			break;
-		}
-		else if (s1[i] != s2[i])
-		{
-			diff = s1[i] - s2[i];
-			break;
-		}
-		else
-			i++;
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++, s2++;
+
 	}
-	return (diff);
+	return (*s1 - *s2);
 }
