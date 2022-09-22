d7f84191e7cb789e997728dfb2ccd92c76dc7132
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-22 14:11:52 -0700

    task

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,13 +3,19 @@
 #include <string.h>
 
 /**
- * reverse_array - reversing aray
- * @arra a
- * @n number of element
- * Return; nothing
+ * reverse_array - function that reverse content of array
+ * @a: array
+ * @n: number of elements
+ * Return: nothing
  */
 
 void reverse_array(int *a, int n);
 {
-
+int tmp, index;
+for (index = n - 1; index > n / 2; index--)
+{
+tmp = a[n - 1 - index];
+a[n - 1 - index] = a[index];
+a[index] = tmp;
+}
 }
