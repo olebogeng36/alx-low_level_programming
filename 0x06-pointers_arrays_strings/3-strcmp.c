6e716df68adf784e96eaabdfa31b53456d963d54
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-22 13:42:41 -0700

    task

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,15 +11,8 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int fire;
+	int results;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-			break;
-		if else(s1[i] == s2[i]
-				continue;
-	}
-	return (0);
+	results = strcmp(s1, s2);
+	return (results);
 }
