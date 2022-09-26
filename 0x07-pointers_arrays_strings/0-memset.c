f2c6be2fd2c14f7773e96842c6b650a78352af3b
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-26 14:41:10 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _memse - fiils the memory block with a constant byte
+ * @s: address to memory block
+ *
+ * @b: char to be used
+ * @n: number of byte to be used
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+even more poiner and array and strings
