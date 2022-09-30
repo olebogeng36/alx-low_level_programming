7a591b95fe4cafad7bcadaa98d59e4c966ee4dff
olebogeng36 <clifordolebogeng@gmail.com>
2022-09-30 16:18:14 -0700

    task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * function main - main function
+ * @argc: count arguments
+ * @argv: argumets
+ * Return main
+ */
+int main(int argc, char *argv[])
+{
+	printf("\"%s\".\n", argv[0]);
+}
---
+++ 0x0A-argc_argv/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the charecter c to stdout
+ * @c: The charecter to print
+ *
+ * Return: on success
+ * on error -1 is returned, and errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
