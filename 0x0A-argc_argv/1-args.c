7cd88df9360e51d103278f33fa14a11bf6843ae9
olebogeng36 <clifordolebogeng@gmail.com>
2022-10-01 16:20:26 -0700

    task

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * Main function - prints the number of arguments passed into it
+ * main - prints the number of arguments passed into it
  * @argc: count argument
  * @argv: agrgumets
  * main return: success
  * Return: 'main'
  */
-int main (int argc, char __attribute__((unused)) *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
-	printf("%d\n", argc -1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
