229968afc5abab64b6282f5b4bb223d56b3908cf
olebogeng36 <clifordolebogeng@gmail.com>
2022-10-14 03:50:04 -0700

    task

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * sum_them_all - prints the sum of all its parameter
+ * @var_list: results
+ * @var_start: results
+ * @var_end: return sum
+ * if n == 0 return 0
+ * Return sum_them_alum_them_all;
+ */
+int sum_them_all(const unsigned int n, ...);
+{
+	var_list ap;
+
+	var_start(ap, args);
+
+	unsigned int n;
+	unsigned int sum = 0;
+
+	for (n = 0; n < args; n++)
+		if (n == 0)
+		{
+			return (0);
+		}
+	sum += var_arg(ap, unsigned int);
+
+	var_end(ap);
+	return (sum);
+}
